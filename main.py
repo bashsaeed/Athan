04412b11ce07cf1a35acb2e33bb6808c0fea0128
Bashar <bwasaeed@gmail.com>
2023-02-10 20:16:48 +0100

    Linking it all together.

--- main.py
+++ main.py
@@ -1,16 +1,68 @@
-# This is a sample Python script.
+import datetime
+import random
+from typing import List
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pause
+from prettytable import PrettyTable
+
+import athanRecordingList
+import athanScrapers
+from athanRecording import AthanRecording
+from athanScrapers import AthanScraper
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main(athan_scraper: AthanScraper, athan_recordings_list: List[AthanRecording]) -> None:
+    while True:
+        time_now = datetime.datetime.now()
+        print()
+        athan_time = athan_scraper.get_athan_times(time_now)
+
+        R = "\033[0;31;40m"  # RED
+        G = "\033[0;32;40m"  # GREEN
+        Y = "\033[0;33;40m"  # Yellow
+        B = "\033[0;34;40m"  # Blue
+        N = "\033[0m"  # Reset
+
+        color = random.choice([R, G, Y, B])
+
+        table = PrettyTable()
+        table.title = color + f"({time_now.strftime('%A')}) {time_now.date()}" + N
+        table.field_names = ["Athan", "Time"]
+        table.add_rows(
+            list(
+                zip(
+                    list(athan_time.index.values),
+                    list(
+                        athan_time.apply(
+                            lambda t: datetime.datetime.strftime(t, "%H:%M")
+                        )
+                    ),
+                )
+            )
+        )
+        print(table)
+
+        athan_time = athan_time[athan_time >= time_now]
+
+        while athan_time.size:
+            next_athan = athan_time[0]
+            pause.until(next_athan)
+
+            athan_sound: AthanRecording = random.choice(athan_recordings_list)
+            athan_sound.play_athan()
+
+            athan_time = athan_time[1:]
+
+        tomorrow = time_now.date() + datetime.timedelta(days=1)
+        tomorrow = datetime.datetime(tomorrow.year, tomorrow.month, tomorrow.day, 1, 0)
+        print(f"Pausing until {tomorrow}", end="\r")
+        pause.until(tomorrow)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    madrid_islamic_center = athanScrapers.MadridIslamicCenter()
+    athan_recordings = [
+        athanRecordingList.SanaaAthan(),
+        # athanRecordingList.DubaiMallAthan()
+    ]
+    main(athan_scraper=madrid_islamic_center, athan_recordings_list=athan_recordings)
